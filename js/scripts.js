f68caae51d6dfbaaa88eabf63c732f5a1a911569
hannah melendy and Kaya Jepson <kaya@>
2019-04-02 10:18:07 -0700

    add if statement to reject negative values, non numbers, and if incrementNum is greater than ceilingNum

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var input1 = $("input#input1").val();
-    var input2 = $("input#input2").val();
-    var ceilingNum = parseInt(input2);
-    var incrementNum = parseInt(input1);
-    // console.log(typeof(incrementNum2));
-    // console.log(typeof(ceilingNum2));
+    var incrementNum = parseInt($("input#input1").val());
+    var ceilingNum = parseInt($("input#input2").val());
+    // var ceilingNum = parseInt(input2);
+    // var incrementNum = parseInt(input1);
+    console.log(typeof(incrementNum));
+    console.log(typeof(ceilingNum));
+    if (incrementNum === ' ' || incrementNum < 0 || incrementNum > ceilingNum || (isNaN(incrementNum)) || ceilingNum === ' ' || ceilingNum < 0 || ceilingNum > ceilingNum || (isNaN(ceilingNum)) ) {
+    alert("Please try again!");
+    }
     for (var index = 0; index <= ceilingNum; index += incrementNum) {
       console.log(index);
     }
